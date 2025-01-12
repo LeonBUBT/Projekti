b004f8162a324775cce549a9a3fdab9b2dddb761
LeonBUBT <lb70655@ubt-uni.net>
2025-01-12 22:16:15 +0100

    removing comments

--- cards.php
+++ cards.php
@@ -4,23 +4,6 @@
 ?>
 <main>
     <div class="wrapper">
-
-        <!-- <div class="card-container">
-            <img src="images/debit-card-test1.png" alt="Classic debit card">
-            <div class="card-container-info">    
-                <h2>Classic Debit Card</h2>
-                <p>
-                    The Nexus Classic Debit Card is simple, secure, and ideal for everyday use. 
-                    With no annual fees, it provides instant access to your funds and essential features like fraud protection. 
-                    A perfect choice for practical banking.
-                    <br>
-                    <button>Apply</button>
-                </p>
-            </div>    
-        </div> -->
-
-
-
         <?php
             require './backend/fetch_cards.php';
             if (count($cardTypes) > 0) {
